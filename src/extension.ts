94e0723abd492e90a3bbed988f43735741faa7d0
karola <k4aro0@gmail.com>
2021-09-22 13:06:31 +0200

    show themes in quick pick added

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,35 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "remove-italics" is now active!');
+interface Theme {
+	id: any,
+	label: any,
+	uiTheme: any,
+	path: any,
+}
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('remove-italics.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Remove Italics!');
+export function activate(context: vscode.ExtensionContext) {
+	let themes = vscode.extensions.all.filter(
+		ext => ext.packageJSON.categories && ext.packageJSON.categories.includes("Themes")
+	);
+	let themesQuickPick: any[] = [];
+	themes.forEach(
+		themeArr => themeArr.packageJSON.contributes.themes.forEach(
+			(theme: Theme) => themesQuickPick.push(theme.label)
+		)
+	);
+
+	let disposable = vscode.commands.registerCommand('remove-italics.removeItalics', async () => {
+		let theme = vscode.window.showQuickPick(
+			themesQuickPick, { placeHolder: "Select Color Theme" }
+		);
+		removeItalics(await theme);
 	});
 
 	context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
+
+function removeItalics(theme: Theme) {
+	let path = theme.path;
+}
