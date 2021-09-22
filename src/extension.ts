3b813c6afbca774ce59e528e3afb9348438640e7
karola <k4aro0@gmail.com>
2021-09-22 15:44:50 +0200

    choose single theme or all extension feature added

--- src/extension.ts
+++ src/extension.ts
@@ -11,24 +11,48 @@ export function activate(context: vscode.ExtensionContext) {
 	let themes = vscode.extensions.all.filter(
 		ext => ext.packageJSON.categories && ext.packageJSON.categories.includes("Themes")
 	);
-	let themesQuickPick: any[] = [];
-	themes.forEach(
-		themeArr => themeArr.packageJSON.contributes.themes.forEach(
-			(theme: Theme) => themesQuickPick.push(theme.label)
-		)
-	);
+	// let themesQuickPick: any[];
+	// themes.forEach(
+	// 	themeArr => themeArr.packageJSON.contributes.themes.forEach(
+	// 		(theme: Theme) => themesQuickPick.push(theme.label)
+	// 	)
+	// );
 
-	let disposable = vscode.commands.registerCommand('remove-italics.removeItalics', async () => {
+	let singleItalics = vscode.commands.registerCommand('remove-italics.removeSingleItalics', async () => {
 		let theme = vscode.window.showQuickPick(
-			themesQuickPick, { placeHolder: "Select Color Theme" }
+			getQuickPick('theme'), { placeHolder: "Select Color Theme" }
 		);
-		removeItalics(await theme);
+		// removeItalics(await theme);
 	});
 
-	context.subscriptions.push(disposable);
+	let multipleItalics = vscode.commands.registerCommand('remove-italics.removeMultipleItalics', async () => {
+		let themes = vscode.window.showQuickPick(
+			getQuickPick('extensions'), { placeHolder: "Select Extension" }
+		);
+		// await themes.forEach((theme: Theme) => {
+		// 	removeItalics(theme);
+		// });
+	});
+
+	function getQuickPick(type: string) {
+		let quickPick: any[] = [];
+		themes.forEach(themeArr => {
+			type === 'extensions' 
+			? quickPick.push(themeArr.packageJSON.displayName) 
+			: themeArr.packageJSON.contributes.themes.forEach((theme: Theme) => {
+				quickPick.push(theme.label);
+			}); 
+		});
+		return quickPick || null;
+	}
+
+	context.subscriptions.push(singleItalics);
+	context.subscriptions.push(multipleItalics);
 }
 
 export function deactivate() { }
+
+
 
 function removeItalics(theme: Theme) {
 	let path = theme.path;
